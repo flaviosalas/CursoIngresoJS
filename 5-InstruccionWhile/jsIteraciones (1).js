efaed403f80e7d6010890d22821aacdee539d2ea
flaviosalas <34449894+flaviosalas@users.noreply.github.com>
2020-02-14 22:17:29 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,29 @@
 function mostrar()
 {
-	alert('iteración while');
+	var i = 1;
 
+	while ( i <= 10) {
+		console.log ( i )
+		i++;
+	}
+
+	console.log ("------------------------")
+	
 
 }//FIN DE LA FUNCIÓN
+
+
+/*
+
+function mostrar()
+{
+	var i = 1;
+
+	while ( i < 5 ) {
+		console.log ("Hola");
+		i = i + 1; // Otras formas: i += 1 o i++ (Sòlo se aplica cuando el valor de la variable es 1)//
+	}
+
+
+}
+*/
